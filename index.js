271d982707e353e2c2407b578bc9d379f0e67329
devsamuelmwangi <hirewebappdeveloper@gmail.com>
2022-03-20 10:51:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+// Moves the DODGER to the left
+let moveDodgerLeft = () => {
+	var left = dodger.style.left
+	left = parseInt(left)
+	dodger.style.left = `${left - 1}px`;
+}
+
+// Moves the DODGER to the right 
+let moveDodgerRight = () => {
+	var left = dodger.style.left
+	left = parseInt(left)
+	dodger.style.left = `${left + 1}px`;
+}
